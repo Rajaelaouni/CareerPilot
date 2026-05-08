951a8ce1b406c61c2d07d580329ad34fd4862abe
Rajaelaouni <raja.elaouni1@gmail.com>
2026-05-08 15:09:03 +0100

    feat: finalisation des fonctionnalités de matching et entretien vocal

--- interview_service/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views_interview import interview_start, interview_end
-
-urlpatterns = [
-    path("interview/start", interview_start, name="interview_start"),
-    path("interview/<uuid:session_id>", interview_end, name="interview_end"),
-]
-
